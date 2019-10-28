3fb5369a85102dab4a31a18ae79104173ea72e54
Victor Hernandez <20.victor.hernandez@ksd.org>
2019-10-28 12:41:32 -0700

    added hp timer script

---
+++ Top-down group project/Assets/Scripts/GameTimeHp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameTimeHp : MonoBehaviour
+{
+    public float levelTime = 120.0f;
+    float timer = 0;
+    public Text TimeText;
+    public void Update()
+    {
+        TimeText.text = "Time: " + timer + "/100";
+        timer += Time.deltaTime;
+        if (timer > levelTime)
+        {
+            SceneManager.LoadScene(
+                SceneManager.GetActiveScene().name);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            levelTime--;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            levelTime--;
+        }
+    }
+}
