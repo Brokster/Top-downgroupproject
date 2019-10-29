4f777fb132b05104f567f5a967b62c600585b25a
Victor Hernandez <20.victor.hernandez@ksd.org>
2019-10-29 13:58:05 -0700

    added enemy animations and shoot scripts

---
+++ Top-down group project/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public Vector2 paceDirection;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector2 ProjectileDirection = new Vector2(player.position.x - transform.position.x,
+                player.position.y - transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
