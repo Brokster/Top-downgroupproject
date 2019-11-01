53a7ba9ba937cf687401de1e968d095c3af9ae7b
Brok Field <20.brok.field@ksd.org>
2019-11-01 13:47:29 -0700

    did some testing in my scene

---
+++ Top-down group project/Assets/Scripts/TextChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextChangeColor : MonoBehaviour
+{
+    public class CollisionColorChange : MonoBehaviour
+    {
+
+        public Color redcolor;
+        public Color bluecolor;
+
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                transform.GetComponent<Renderer>().material.color = bluecolor;
+            }
+        }
+    }   
+}
